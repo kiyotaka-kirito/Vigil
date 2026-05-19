3095dfc3be42f073c244189bf13eaefc0c67cfc4
kiyotaka-kirito <mghtetpai.1999@gmail.com>
2026-05-19 15:45:50 +0700

    Initial commit

---
+++ VigilApp/VigilApp/VigilAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VigilAppApp.swift
+//  VigilApp
+//
+//  Created by Kiyotaka Kirito on 19/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct VigilAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
