4b4b352b7f4963c99bdaa981fb40e95d44c5e2f3
kiyotaka-kirito <mghtetpai.1999@gmail.com>
2026-05-19 16:39:18 +0700

    feat: add enum Category to use in model, and then add Budget and Transaction for model

---
+++ VigilCore/Sources/VigilCore/Model/Budget.swift
@@ -0,0 +1,36 @@
+//
+//  Budget.swift
+//  VigilCore
+//
+//  Created by Kiyotaka Kirito on 19/05/2026.
+//
+
+import Foundation
+
+public struct Budget: Identifiable {
+    
+    public let id: UUID
+    public let category: Category
+    public let monthlyLimit: Double
+    public let amountSpent: Double
+    
+    public init(
+        id: UUID = UUID(),
+        category: Category,
+        monthlyLimit: Double,
+        amountSpent: Double = 0
+    ) {
+        self.id             = id
+        self.category       = category
+        self.monthlyLimit   = monthlyLimit
+        self.amountSpent    = amountSpent
+    }
+    
+    public var remaining: Double {
+        monthlyLimit - amountSpent
+    }
+    
+    public var isOverBudget: Bool {
+        amountSpent > monthlyLimit
+    }
+}
